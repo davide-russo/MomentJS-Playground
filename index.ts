3cdb725f553071dc342328efaff2b377e2d5adc8
davide-russo <davide.russo.genova@gmail.com>
2020-03-14 19:44:40 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import './style.css';
+import moment from 'moment';
 
-import moment from 'moment'
+const myFormat: string = 'DD/MM/YYYY HH:mm';
 
-const now = moment();
+const a = moment();
+const b = moment();
+
+const result = a < b;
 
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `
-<h1>TypeScript Starter</h1>
-
-Today: ${now.format()} <br>
-ISO Weekday: ${now.isoWeekday()}
-`;
-
+appDiv.innerHTML = `<h1>MomentJs Playground - ${moment().format(myFormat)}</h1><br>
+                    <p>A: ${a.format(myFormat)}</p>
+                    <p>B: ${b.format(myFormat)}</p>
+                    <p>Result: ${result}</p>`;
